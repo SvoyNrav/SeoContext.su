4390ed3381b939a9f400375bb91db007ba4fd153
svoyn <svoynrav@yandex.ru>
2015-11-16 13:25:26 +0300

    Блок "Наши сертификаты"

--- sect_area_5.php
+++ sect_area_5.php
@@ -5,7 +5,7 @@
           <h2 class="anim-fade-up">
 
               <!-- MAIN TITLE -->
-              Наши партнеры<br/>
+              Наши сертификаты<br/>
 
               <!-- SUBTITLE -->
               <span>Digital-агенство SeoContext</span>
@@ -16,16 +16,16 @@
           <div class="row carousel-logos" id="carousel-logos">
             
               <!-- LOGO 1 -->
-              <a href="#"><div class="col-md-10"><img src="<?=SITE_TEMPLATE_PATH?>/img/logos/01.png" alt></div></a>
+              <a href="#"><div class="col-md-10"><img src="<?=SITE_TEMPLATE_PATH?>/img/logos/01.png" alt="SeoContext - cертифицированное агенство Яндекс.Директ"></div></a>
               
               <!-- LOGO 2 -->
-              <a href="#"><div class="col-md-10"><img src="<?=SITE_TEMPLATE_PATH?>/img/logos/02.png" alt></div></a>
+              <a href="#"><div class="col-md-10"><img src="<?=SITE_TEMPLATE_PATH?>/img/logos/02.png" alt="SeoContext - cертифицированный партнер Google"></div></a>
               
               <!-- LOGO 3 -->
-              <a href="#"><div class="col-md-10"><img src="<?=SITE_TEMPLATE_PATH?>/img/logos/03.png" alt></div></a>
+              <a href="#"><div class="col-md-10"><img src="<?=SITE_TEMPLATE_PATH?>/img/logos/03.png" alt="SeoContext - cертифицированный партнер Битрикс"></div></a>
               
               <!-- LOGO 4 -->
-              <a href="#"><div class="col-md-10"><img src="<?=SITE_TEMPLATE_PATH?>/img/logos/04.jpg" alt></div></a>
+              <a href="#"><div class="col-md-10"><img src="<?=SITE_TEMPLATE_PATH?>/img/logos/04.png" alt="SeoContext - аттестованное агенство Mail.ru"></div></a>
 
           </div>
           
